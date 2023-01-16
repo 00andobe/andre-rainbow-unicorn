7c7d361e604c77ba9897e994980e9cf9cc014cff
André Oberg <oberg@andre.local>
2023-01-16 12:00:56 +0100

    Loop addad

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 var animals = ["apa", "orangutang", "gorilla", "get", "giraff"];
 console.log(animals);
+var objectTest = {
+    string: "String",
+    boolean: true,
+    number: 29
+};
+console.log(objectTest);
+var returnString = function (x) {
+    return "hej" + x;
+};
+console.log(returnString("x"));
+for (var i = 0; i < animals.length; i++) {
+    console.log(returnString(animals[i]));
+}
--- app.ts
+++ app.ts
@@ -1,2 +1,25 @@
 const animals: string[] = [ "apa", "orangutang", "gorilla", "get", "giraff"];
 console.log(animals);
+
+var objectTest = {
+    string: "String",
+    boolean: true,
+    number: 29
+  }
+  console.log(objectTest);
+
+  const returnString = function (x:string) {
+	return "hej" + x
+}
+console.log(returnString("x"));
+
+
+
+
+
+
+
+
+ for(let i = 0; i < animals.length; i++) {
+ 	console.log(returnString(animals[i]));
+ }
